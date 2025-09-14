c2f44e6db04d4e49f4a91e0a56d1882cf278e440
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2025-09-14 14:50:02 -0500

    Update project version to 2.1.0

--- uninstall_debian.sh
+++ uninstall_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Fail2ban Banned IPs Monitor - Debian Uninstallation Script
-# Version: v2.0.3
+# Version: v2.1.0
 # Description: Automated uninstallation script for Debian/Ubuntu systems
 # Date: August 2025
 #
